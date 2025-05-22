41355c5c65d2d21ed53213e987bd6363665470d2
tarekazz1 <tarekazzi@outlook.com>
2025-05-22 13:49:16 -0400

    chore(cleanup): remove unused mobileNav.js and its import

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,9 @@
 import { initTypewriter } from './typewriter.js';
 import { initToggleButtons } from './toggleButtons.js';
 import { initParticles } from './particles.js';
-import { initMobileNav } from './mobileNav.js';
 
 document.addEventListener('DOMContentLoaded', function() {
   initTypewriter();
   initToggleButtons();
   initParticles();
-  initMobileNav();
 });
